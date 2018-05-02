33b17129481d8d7e241be0076df13c4bdf7296ca
Jathmel Gordon <jathmel@gmail.com>
2018-05-02 05:10:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.join
+  elsif array.size == 2
+    array.insert(-2, 'and').join(' ')
+  else
+     array.insert(-2, 'and')
+    new_arr = []
+    array.each do |el|
+      break if el == 'and'
+      el << ','
+    end
+    array.join(' ')
+  end
 end
